7419f4ea80272569250f4ec19cc19de19dcdc771
minniexp <minyyang35@gmail.com>
2023-03-14 18:39:35 -0500

    update database

--- database.js
+++ database.js
@@ -2,8 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "finalcheck_user_user",
-  // password: process.env.REACT_APP_DATABASE_PASSWORD,
-  password: "T6Lyt0MfLZLdTNuceo3DXlDopSuQ7ErK",
+  password: process.env.REACT_APP_DATABASE_PASSWORD,
   host: "dpg-cg7al71mbg5ab7nkv110-a.ohio-postgres.render.com",
   port: 5432,
   database: "finalcheck_user",
