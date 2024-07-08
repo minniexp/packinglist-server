84c980e436d33d48bb2819076cff8322f75d5b6d
minniexp <98223907+minniexp@users.noreply.github.com>
2024-07-07 22:29:11 -0500

    Update database.js
    update user

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "finalcheckv2_user",
+  user: "finalcheckdb_user",
   password: process.env.REACT_APP_DATABASE_PASSWORD,
   host: "dpg-cq5lmjmehbks73bqnqv0-a.ohio-postgres.render.com",
   port: 5432,
